32b71b5b1eb41ec18ed2dd19ac1ab42de1caa24d
dotmark-g <59557120+dotmark-g@users.noreply.github.com>
2020-01-07 22:07:46 +0900

    Add files via upload

--- Grad Thesis Code.R
+++ Grad Thesis Code.R
@@ -19,18 +19,29 @@ lmDF<- lm(St.Reply ~ (St.RT + maincategory)^2 + paid + dependent + include_deliv
 summary(lmDF)
 
 
-
-
 #以下リプライの感情分析
 
 #形態素解析・感情値代入済みのデータを読み込む
 maResultDF <- readRDS("./outputDF/maResultDF.rds")
 maResultDF %<>% left_join(MediaNamesList, by = c("target" = "screen_name"))
-
+maResultDF %<>% ungroup()
 
 maResultDF %>% mutate(Std.Emotion = scale(Emotion_Value), Std.Term = scale(Term_Count)) -> Std.maResult
 
 #感情値について、重回帰分析をおこなう
-summary(lm(formula = Std.Emotion ~ Std.Term * 
-             +              (maincategory + paid + dependent + include_delivery + domicile), data = Std.maResult))
+summary(lm(formula = Emotion_Value ~ Term_Count * (maincategory + paid + dependent + include_delivery + domicile), data = Std.maResult))
+
+
+#形態素数と感情値の密度分布を描画する
+maResultDF %>% ggplot() +
+  geom_density2d(aes(x = Term_Count, y = Emotion_Value))
+
+
+#形態素数の密度曲線を描画する
+maResultDF %>%
+  mutate(hard = ifelse(maincategory %in% c("economy", "general", "local"),
+                       "hard", "other")) %>% 
+  ggplot() +
+  geom_density(aes(x = Term_Count, fill = hard, alpha = 0.5))
+
 
